c7f3c8ff2ae913125ad9c27cb95069d0ea391cfd
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2016-05-17 14:40:01 -0500

    Update haproxy.sh
    Correcciones en el archivo.

--- haproxy.sh
+++ haproxy.sh
@@ -8,3 +8,4 @@ sudo apt-get install -y haproxy
 sudo sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/haproxy
 sudo mv /etc/haproxy/haproxy.cfg{,.original}
 sudo cp -f /vagrant/haproxy.cfg /etc/haproxy/
+sudo service haproxy start
